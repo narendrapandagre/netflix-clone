b9e4feb54fb8f5285a7587bb6eb3cb65fa6ac6ed
naren <narendrapanda22@gmail.com>
2025-07-11 00:14:09 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Home from './pages/Home/Home'
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Player from './pages/Player/Player'
-import Search from './pages/Search/Search'
-import Mylist from './pages/MyList/Mylist'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
+import Search from './pages/Search/Search';
+import Mylist from './pages/MyList/Mylist';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter basename="/netflix-clone">
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/player/:id' element={<Player />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/mylist' element={<Mylist />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/mylist" element={<Mylist />} />
       </Routes>
-    </div>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
