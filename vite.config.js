b7b3d9600bbe3dd335b4fc4ee5a13088af8a6d83
naren <narendrapanda22@gmail.com>
2025-07-11 00:03:21 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/netflix-clone/', // <-- ADD THIS LINE (this is your repo name)
 })
